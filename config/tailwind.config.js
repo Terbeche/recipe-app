26e2319c499c91a7016e8e5f443fb0412de537c5
terbeche <termus96@gmail.com>
2022-10-08 20:01:44 +0100

    Edit tailwind.congig

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('flowbite/plugin')
+    // require('flowbite/plugin')
+    require('@themesberg/flowbite/plugin')
+
   ]
 }
